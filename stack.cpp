7ca42af827ffa3b25c6c1bb701a7330c939e7fbb
Compotation <46853874+Compotation@users.noreply.github.com>
2023-05-02 00:19:26 -0400

    comment out errors

--- stack.cpp
+++ stack.cpp
@@ -1,26 +1,26 @@
 #include "stack.h"
 
 void stack::push(std::string item){
-  if (topindex >= 5){
-    throw STACK_ERR_FULL;
-  }
+//  if (topindex >= 5){
+//    throw STACK_ERR_FULL;
+//  }
   
   helperList->insert(item);
 }
 
 std::string stack::pop(){
-  if (topindex <= 0){
-    throw STACK_ERR_EMPTY;
-  }
+//  if (topindex <= 0){
+//    throw STACK_ERR_EMPTY;
+//  }
 
   helperList->remove(0);
   return helperList->getHead()->getData();
 }
 
 std::string stack::top(){
-  if (topindex <= 0){
-    throw STACK_ERR_EMPTY;
-  }
+//  if (topindex <= 0){
+//    throw STACK_ERR_EMPTY;
+//  }
   
   return helperList->getHead()->getData();
 }
