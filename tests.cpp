1577376f1b932989303bb041b01442223cc0428b
Compotation <46853874+Compotation@users.noreply.github.com>
2023-05-02 01:31:14 -0400

    enqueue test

--- tests.cpp
+++ tests.cpp
@@ -2,6 +2,7 @@
 
 #include "doctest.h"
 #include "stack.h"
+#include "Queue.h"
 
 TEST_CASE("push") {
   stack *s = new stack();
@@ -31,4 +32,12 @@ TEST_CASE("top") {
   stack *s = new stack();
   s->push("hi");
   CHECK_EQ(s->top(), "hi");
+}
+
+TEST_CASE("enqueue") {
+  Queue *q = new Queue();
+  q->enqueue(2);
+  q->enqueue(3);
+  q->enqueue(10);
+  CHECK_EQ(q->toString(), "2,3,10");
 }
